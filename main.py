6e02ebff3b82ddba4c8d009a5564479749be454b
a1 <4267817@mail.ru>
2023-08-28 21:48:33 +0300

    homework-6

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from src.keyboard import Keyboard
+from src.item import Item
 
-if __name__ == "__main__":
-    kb = Keyboard("Dark Project KD87A", 9600, 5)
-    assert str(kb) == "Dark Project KD87A"
+if __name__ == '__main__':
+    # Файл items.csv отсутствует.
+    Item.instantiate_from_csv()
+    # FileNotFoundError: Отсутствует файл item.csv
 
-    assert str(kb.language) == "EN"
-
-    kb.change_lang()
-    assert str(kb.language) == "RU"
-
-    # Сделали RU -> EN -> RU
-    kb.change_lang().change_lang()
-    assert str(kb.language) == "RU"
-
-    kb.language = "CH"
-    # AttributeError: property 'language' of 'Keyboard' object has no setter
+    # В файле items.csv удалена последняя колонка.
+    Item.instantiate_from_csv()
+    # InstantiateCSVError: Файл item.csv поврежден
--- tests/test_item.py
+++ tests/test_item.py
@@ -35,12 +35,20 @@ def test_apply_discount(item_1):
     assert item_1.apply_discount() * 0.8 == 8000.0
 
 
-def test_instantiate_from_csv(item_1):
+def test_instantiate_from_csv():
     Item.instantiate_from_csv()
     assert len(Item.all) == 5
 
 
-def test_string_to_number(item_1):
+def test_string_to_number():
     assert Item.string_to_number("5") == 5
     assert Item.string_to_number("5.0") == 5
     assert Item.string_to_number("5.5") == 5
+
+
+def test_item_instantiate_from_csv_raises():
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv()
+
+    with pytest.raises(Item.InstantiateCSVError):
+        Item.instantiate_from_csv()
